41b3d0f6f215a28040e821bf8fa46c5a5eb7ecf4
tfettrow <tfettrow@gmail.com>
2019-09-16 15:55:36 -0400

    replaced file_organize with MMs paralled version... soo nice

--- file_organize.sh
+++ file_organize.sh
@@ -1,10 +1,39 @@
+
+subjects=(CrunchPilot01_development1)
+
 #####################################################################################
 ml mricrogl
 ml gcc/5.2.0
 ml pigz
 
 
-subjects=(CrunchPilot01_development1)
+convertDICOM(){
+	this_raw_folder_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f1)
+	this_processed_folder_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f2)
+	this_processed_file_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f3)
+	mkdir -p "${Subject_dir}/Processed/MRI_files/${this_processed_folder_name}"
+
+	cd ${Subject_dir}/Raw/MRI_files/$this_raw_folder_name
+	if [ -e *.nii ]; then 
+		rm *.nii.gz*
+		rm *.nii*
+		rm *.json*
+	fi 
+	
+	dcm2niix -ba n ${Subject_dir}/Raw/MRI_files/$this_raw_folder_name
+
+	for nii_file in *.nii*; do
+		mv -v ${nii_file} $this_processed_file_name.nii
+		cp $this_processed_file_name.nii "${Subject_dir}/Processed/MRI_files/${this_processed_folder_name}";
+	done
+
+	for json_file in *.json*; do
+		mv -v ${json_file} $this_processed_file_name.json
+		cp $this_processed_file_name.json "${Subject_dir}/Processed/MRI_files/$this_processed_folder_name";
+	done
+	cd $Subject_dir
+}
+
 
 for SUB in ${subjects[@]}; do
 
@@ -14,31 +43,8 @@ for SUB in ${subjects[@]}; do
 	
 	number_of_folders_to_extract=$(cat ${SUB}_file_information.csv | wc -l)
 	for (( this_folder_row=1; this_folder_row<=${number_of_folders_to_extract}; this_folder_row++ )); do
-		this_raw_folder_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f1)
-		this_processed_folder_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f2)
-		this_processed_file_name=$(cat ${SUB}_file_information.csv | sed -n ${this_folder_row}p | cut -d ',' -f3)
-
-		mkdir -p "${Subject_dir}/Processed/MRI_files/${this_processed_folder_name}"
-	
-		cd ${Subject_dir}/Raw/MRI_files/$this_raw_folder_name
-		if [ -e *.nii ]; then 
-			rm *.nii.gz*
-			rm *.nii*
-			rm *.json*
-		fi 
-		
-	
-		dcm2niix -ba n ${Subject_dir}/Raw/MRI_files/$this_raw_folder_name
-	
-		for nii_file in *.nii*; do
-			mv -v ${nii_file} $this_processed_file_name.nii
-			cp $this_processed_file_name.nii "${Subject_dir}/Processed/MRI_files/${this_processed_folder_name}";
-		done
-	
-		for json_file in *.json*; do
-			mv -v ${json_file} $this_processed_file_name.json
-			cp $this_processed_file_name.json "${Subject_dir}/Processed/MRI_files/$this_processed_folder_name";
-		done
-		cd $Subject_dir
+		convertDICOM $SUB $this_folder_row &
 	done
+	wait
 done
+echo "This script took $SECONDS seconds to execute"
