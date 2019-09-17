7c2da9d84b4284bdfc3b28779db0c1de2a34c504
Michael-Monaldi <42719165+Michael-Monaldi@users.noreply.github.com>
2019-09-17 17:11:03 -0400

    commented out and changed file names

--- run_all_scripts.sh
+++ run_all_scripts.sh
@@ -1,17 +1,17 @@
-cd /ufrc/davidclark/share/MiM_Code/MiM_Shell_Scripts
-#cd /ufrc/rachaelseidler/tfettrow/Crunch_Code/MiM_Shell_Scripts
+#cd /ufrc/davidclark/share/MiM_Code/MiM_Shell_Scripts
+cd /ufrc/rachaelseidler/tfettrow/Crunch_Code/MiM_Shell_Scripts
 #####################################################################################
 #mkdir -p ${Subject_dir}/Processed/MRI_files/01_Localizer
-./cleanup.sh
-echo "cleanup done"
+#./cleanup.sh
+#echo "cleanup done"
 
 ./generate_raw_file_info.sh
 echo "csv information generated"
 
-./test_organize.sh
-echo "test_organize done"
+./file_organize.sh
+echo "file_organize done"
 
-./test_preprocess_fmri.sh
+./preprocess_fmri.sh
 echo "preprocessing done"
 
 echo "THE WHOLE THING (COMPILATION) TOOK $SECONDS seconds TO FINISH"
