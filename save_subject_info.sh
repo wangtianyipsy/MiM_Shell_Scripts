e74b4986612c4dd49bcb109723e141bb9f7e3458
tfettrow <tfettrow@gmail.com>
2019-12-11 09:51:33 -0500

    new scripts for saving subject info and for finding betas across conditions

---
+++ save_subject_info.sh
@@ -0,0 +1,29 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+subjects=(2002)
+
+
+Code_dir=/ufrc/rachaelseidler/tfettrow/Crunch_Code
+
+
+export MATLABPATH=${Code_dir}/Matlab_Scripts/helper
+
+ml matlab
+
+
+for SUB in ${subjects[@]}; do
+
+	Subject_dir=/ufrc/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data/${SUB}
+	cd ${Subject_dir}/Raw/MRI_files/T1_MPRAGE_SAG_ISO_8MM_0006
+	matlab -nodesktop -nosplash -r "try; save_subject_info; catch; end; quit"
+	echo $SUB info saved took $SECONDS seconds to execute
+done
