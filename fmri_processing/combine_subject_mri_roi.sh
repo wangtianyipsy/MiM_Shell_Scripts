6d048286e448791c66a410402c9837ed1889114b
tfettrow <tfettrow@gmail.com>
2021-01-20 16:39:11 -0500

    cleaning up roi vs wholebrain scripts

--- fmri_processing/combine_subject_mri_roi.sh
+++ fmri_processing/combine_subject_mri_roi.sh
@@ -9,13 +9,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# this script requires arguments 
-
+# this script requires arguments... use the batch_fmri.batch to call this shell script
 # example >> combine_subject_mri_redcap.sh '1002,1004,1007,1009,1010,1011,1013,2021,2015,2002,2018,2017,2012,2025,2020,2026,2023,2022,2007,2013,2008,2033,2034,2037' 05_MotorImagery 
 
 argument_counter=0
 for this_argument in "$@"; do
-	if	[[ $argument_counter == 0 ]]; then
+	if [[ $argument_counter == 0 ]]; then
 		subjects=$this_argument
 	elif [[ $argument_counter == 1 ]]; then
 		fmri_processed_folder_names=$this_argument
@@ -23,30 +22,26 @@ for this_argument in "$@"; do
 	(( argument_counter++ ))
 done
 
-Code_dir=/blue/rachaelseidler/tfettrow/Crunch_Code
-export MATLABPATH=${Code_dir}/Matlab_Scripts/helper
-
 #for SUB in ${subjects[@]}; do
 Study_dir=/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data
 cd $Study_dir
-
-
+echo "CD set"
 for this_functional_run_folder in ${fmri_processed_folder_names[@]}; do # only doing one task folder at a time so this for loop not necessary
 	ml fsl
 	subject_index=0
 
-	outfile=${this_functional_run_folder}_fmri_redcap.csv
+	outfile=${this_functional_run_folder}_fmri_roi_betas_all.csv
 
-	if [ -e ${this_functional_run_folder}_fmri_redcap.csv ]; then
-		rm ${this_functional_run_folder}_fmri_redcap.csv
+	if [ -e ${this_functional_run_folder}_fmri_roi_betas_all.csv ]; then
+		rm ${this_functional_run_folder}_fmri_roi_betas_all.csv
 	fi
 
 	while IFS=',' read -ra subject_list; do
    	    for this_subject in "${subject_list[@]}"; do
    	    	cd ${Study_dir}/$this_subject/Processed/MRI_files/${this_functional_run_folder}/ANTS_Normalization/Level1_WholeBrain
 
-   	    	this_subject_header=$(cat ${this_subject}_fmri_redcap.csv | sed -n 1p)
-   	    	this_subject_data=$(cat ${this_subject}_fmri_redcap.csv | sed -n 2p)
+   	    	this_subject_header=$(cat ${this_subject}_fmri_roi_betas.csv | sed -n 1p)
+   	    	this_subject_data=$(cat ${this_subject}_fmri_roi_betas.csv | sed -n 2p)
 			
 			cd ${Study_dir}
    	    	this_subject_header_outfile=$(cat $outfile | sed 1d)
