5152c2f93aa3a6a6d3fbd8d2835633d0a80a1313
tfettrow <tfettrow@gmail.com>
2021-02-10 10:40:15 -0500

    added study folder arg for batch_struct

--- struct_processing/structural_analysis.sh
+++ struct_processing/structural_analysis.sh
@@ -1,20 +1,51 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# this script requires arguments 
+
+# example >> structural_analysis.sh $Matlab_dir '1002,1004,1007,1009,1010,1011,1013,1020,1022,1024,1027,2002,2007,2008,2012,2013,2015,2017,2018,2020,2021,2022,2023,2025,2026,2033,2034,2037,2042,2052'
 
 argument_counter=0
 for this_argument in "$@"
 do
-	# if	[[ $argument_counter == 0 ]]; then
- #    	subject=$this_argument
-	# else
-		processing_steps="$this_argument"
-	# fi
+	if	[[ $argument_counter == 0 ]]; then
+		Matlab_dir=$this_argument
+	elif [[ $argument_counter == 1 ]]; then
+    	Template_dir=$this_argument
+    elif [[ $argument_counter == 1 ]]; then
+    	Study_dir=$this_argument
+    elif [[ $argument_counter == 3 ]]; then
+    	subject=$this_argument
+    else
+		struct_processing_steps="$this_argument"
+	fi
+	(( argument_counter++ ))
+done
+export MATLABPATH=${Matlab_dir}/helper
 	
-	# Set the path for our custom matlab functions and scripts
-	Code_dir=/blue/rachaelseidler/tfettrow/Crunch_Code
-	
-	export MATLABPATH=${Code_dir}/Matlab_Scripts/helper
-	
-	Subject_dir=/blue/rachaelseidler/share/FromExternal/Research_Projects_UF/CRUNCH/MiM_Data/${subject}
-	cd "${Subject_dir}"
+# ml matlab/2020a
+# matlab -nodesktop -nosplash -r "try; cat12StructuralAnalysis('subjects',{'1002','1004','1007','1009','1010','1011','1013','1020','1022','1024','1027','2002','2007','2008','2012','2013','2015','2017','2018','2020','2021','2022','2023','2025','2026','2033','2034',2037','2042','2052','3004','3006','3007','3008'},'t1_folder','02_T1','t1_filename','T1.nii','steps_to_run_vector',[1 0 1 0 1],'template_dir','/blue/rachaelseidler/tfettrow/Crunch_Code/MR_Templates'); catch; end; quit"
+# ERROR (could not find Cat_log.)
+
+# ml matlab/2020a
+# matlab -nodesktop -nosplash -r "try; cat12StructuralAnalysis('subjects',{$subject}','t1_folder','02_T1','t1_filename','T1.nii','steps_to_run_vector',[1 0 0 0 0],'template_dir','/blue/rachaelseidler/tfettrow/Crunch_Code/MR_Templates'); catch; end; quit"
+
+Subject_dir=$Study_dir/$subject
+
+	export MATLABPATH=${Matlab_dir}/helper
+	ml matlab/2020a
+	ml gcc/5.2.0; ml ants ## ml gcc/9.3.0; ml ants/2.3.4
+	ml fsl/6.0.1
+	ml itksnap
+	cd $Subject_dir
 
 	lines_to_ignore=$(awk '/#/{print NR}' file_settings.txt)
 
@@ -35,7 +66,7 @@ do
 	for item_to_remove_t1 in ${remove_this_item_t1[@]}; do
 		t1_line_numbers_to_process=$(echo ${t1_line_numbers_to_process[@]/$item_to_remove_t1})
 	done
-	
+
 	this_index=0
 	for this_line_number in ${t1_line_numbers_to_process[@]}; do
 		t1_processed_folder_name_array[$this_index]=$(cat file_settings.txt | sed -n ${this_line_number}p | cut -d ',' -f2)
@@ -43,20 +74,12 @@ do
 	done
 	
 	t1_processed_folder_names=$(echo "${t1_processed_folder_name_array[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-	
-	for this_preprocessing_step in "${processing_steps[@]}"; do
-		if [[ $this_preprocessing_step == "cat12" ]]; then
-			this_t1_folder=($t1_processed_folder_names)
-		    mkdir -p ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/CAT12_Analysis
-		    #cat12('expert')
-			cp ${Code_dir}/MR_Templates/Template_*.nii ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/CAT12_Analysis
-			cp ${Code_dir}/MR_Templates/TPM.nii ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/CAT12_Analysis
-			cp ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/T1.nii ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/CAT12_Analysis
-			cd ${Subject_dir}/Processed/MRI_files/${this_t1_folder}/CAT12_Analysis
-			ml matlab/2020a
-			matlab -nodesktop -nosplash -r "try; cat12StructuralAnalysis('subjects',{'1002','1004','2007','2008'},'t1_folder','02_T1','t1_filename','T1.nii','steps_to_run_vector',[1 0 0 0 0]); catch; end; quit"
-		fi
 
+
+	for this_preprocessing_step in "${struct_processing_steps[@]}"; do
+		if [[ $this_preprocessing_step ==  "check_struct_segments" ]]; then
+			data_folder_to_analyze=($t1_processed_folder_names)
+			echo checking $subject
+			itksnap -g ${Study_dir}/ROIs/MNI_2mm.nii -o ${Subject_dir}/Processed/MRI_files/${data_folder_to_analyze}/CAT12_Analysis/mri/mwp1T1.nii
+		fi
 	done
-	(( argument_counter++ ))
-done
